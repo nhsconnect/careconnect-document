94829acfef10fec2d7f0316d905508de0dc6e30c
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2018-04-09 11:23:10 +0100

    EDMS Mock on (Apache ) CMIS enabled systems

--- ccri-edms-server/src/main/java/uk/nhs/careconnect/edms/providers/BinaryProvider.java
+++ ccri-edms-server/src/main/java/uk/nhs/careconnect/edms/providers/BinaryProvider.java
@@ -6,18 +6,22 @@ import ca.uhn.fhir.rest.annotation.*;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 
 
+import org.apache.chemistry.opencmis.client.api.*;
+import org.apache.chemistry.opencmis.commons.data.ContentStream;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
 import org.hl7.fhir.dstu3.model.*;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.chemistry.opencmis.client.api.Session;
-
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import sun.misc.IOUtils;
+import uk.nhs.careconnect.edms.OpenCMIS;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 
 @Component
@@ -30,8 +34,9 @@ public class BinaryProvider implements IResourceProvider {
 
     public static final String CMIS_URL = "/public/cmis/versions/1.1/atom";
 
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BinaryProvider.class);
 
-
+    Session session;
 
     public Class<? extends IBaseResource> getResourceType() {
         return Binary.class;
@@ -39,15 +44,64 @@ public class BinaryProvider implements IResourceProvider {
 
 
     @Read
-    public Composition getCompositionById(HttpServletRequest request, @IdParam IdType internalId) {
+    public Binary getBinaryById(HttpServletRequest request, @IdParam IdType internalId) {
 
-       // Session cmisSession = getCmisSession();
+        Binary binary = null;
+        OpenCMIS openCMIS = new OpenCMIS();
+        session = openCMIS.getSession("fhir","test","test");
 
+        //openCMIS.listTopFolder(session);
+        //Folder root = session.getRootFolder();
+        //getFolderContents(root, "/");
+        log.info("Get Binary /"+internalId.getValue());
+        try {
+            CmisObject fileObj = session.getObject(internalId.getValue());
+            log.info("Name = "+fileObj.getName());
+            if (fileObj instanceof Document) {
+                Document docMetadata = (Document) fileObj;
+                ContentStream docContent = docMetadata.getContentStream();
+                binary = new Binary();
+                binary.setContentType(docContent.getMimeType());
+                binary.setId(internalId.getValue());
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                try {
+                    org.apache.commons.io.IOUtils.copyLarge(docContent.getStream(),buffer);
+                    byte[] byteArray = buffer.toByteArray();
+                    binary.setContent(byteArray);
+                } catch (IOException ex) {
+                    binary = null;
+                }
+            }
 
-        return null;
+        } catch (CmisObjectNotFoundException obj) {
+            log.info("CMIS Object not found");
+        }
+
+        return binary;
     }
 
+    Folder getFolderContents(Folder folder, String path) {
 
+        ItemIterable<CmisObject> contentItems = folder.getChildren();
+        for (CmisObject contentItem : contentItems) {
+            if (contentItem instanceof Document) {
+                Document docMetadata = (Document) contentItem;
+                ContentStream docContent = docMetadata.getContentStream();
+                log.info("["+path+"] :"+docMetadata.getName() + " id="+ docMetadata.getId() + " [size=" + docContent.getLength() + "][Mimetype=" +
+                        docContent.getMimeType() + "][type=" + docMetadata.getType().getDisplayName() + "]");
+            } else if (contentItem instanceof Folder) {
+                log.info("["+path+"] :"+contentItem.getName() + " [type=" + contentItem.getType().getDisplayName() + "]");
+                if (contentItem.getName().equals("Sites")) { getFolderContents((Folder) contentItem, path+"Sites/"); }
+                if (contentItem.getName().equals("fhir")) { getFolderContents((Folder) contentItem,path+"fhir/" ); }
+                if (contentItem.getName().equals("documentLibrary")) { getFolderContents((Folder) contentItem,path+"documentLibrary/" ); }
+            }
+            else {
+                log.info("["+path+"] :"+contentItem.getName() + " [type=" + contentItem.getType().getDisplayName() + "]");
+                log.info("["+path+"] :"+contentItem.getName() + " [type=" + contentItem.getType().getClass().getCanonicalName() + "]");
+            }
+        }
+        return folder;
+    }
 
 
 }
