43286eebefb18fb68b9c090d24597df705ece1b0
KevinMayfield <mayfield.g.kev@gmail.com>
2018-05-02 22:12:51 +0100

    Smart option

--- document-review-app/src/app/modules/composition-load/load-document.component.ts
+++ document-review-app/src/app/modules/composition-load/load-document.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-load-document',
@@ -8,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoadDocumentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+
+  // https://stackoverflow.com/questions/40214772/file-upload-in-angular
+
+  apiEndPoint : string;
+
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      let formData: FormData = new FormData();
+      formData.append('uploadFile', file, file.name);
+      let headers = new Headers();
+      /** In Angular 5, including the header Content-Type can invalidate your request */
+      headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Accept', 'application/json');
+      //let options = new RequestOptions({headers: headers});
+      /*
+      this.http.post(`${this.apiEndPoint}`, formData, options)
+        .map(res => res.json())
+        .catch(error => Observable.throw(error))
+        .subscribe(
+          data => console.log('success'),
+          error => console.log(error)
+        )
+    }*/
+    }
+  }
+
 
 }
