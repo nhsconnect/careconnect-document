6441373081407274ea4df0e4a10be0cc54ec015c
KevinMayfield <mayfield.g.kev@gmail.com>
2018-06-14 22:39:46 +0100

    Angular 6 conversion

--- document-review-app/nginx/entryPoint.sh
+++ document-review-app/nginx/entryPoint.sh
@@ -11,21 +11,21 @@ echo /usr/share/nginx/html/cat/main*bundle.js
 
 ls /usr/share/nginx/html/cat
 
-sed -i "s/KEYCLOAK_CLIENT_SECRET/$KEYCLOAK_CLIENT_SECRET/g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s/KEYCLOAK_CLIENT_SECRET/$KEYCLOAK_CLIENT_SECRET/g" /usr/share/nginx/html/cat/main.js
 
-sed -i "s/KEYCLOAK_CLIENT_ID/$KEYCLOAK_CLIENT_ID/g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s/KEYCLOAK_CLIENT_ID/$KEYCLOAK_CLIENT_ID/g" /usr/share/nginx/html/cat/main.js
 
-sed -i "s/KEYCLOAK_REALM/$KEYCLOAK_REALM/g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s/KEYCLOAK_REALM/$KEYCLOAK_REALM/g" /usr/share/nginx/html/cat/main.js
 
-sed -i "s~KEYCLOAK_AUTH_URL~$KEYCLOAK_AUTH_URL~g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s~KEYCLOAK_AUTH_URL~$KEYCLOAK_AUTH_URL~g" /usr/share/nginx/html/cat/main.js
 
-sed -i "s~KEYCLOAK_SERVER_URL~$KEYCLOAK_SERVER_URL~g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s~KEYCLOAK_SERVER_URL~$KEYCLOAK_SERVER_URL~g" /usr/share/nginx/html/cat/main.js
 
-sed -i "s/CAT_COOKIE_DOMAIN/$CAT_COOKIE_DOMAIN/g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s/CAT_COOKIE_DOMAIN/$CAT_COOKIE_DOMAIN/g" /usr/share/nginx/html/cat/main.js
 
-sed -i "s/SMART_OAUTH2_CLIENT_SECRET/$SMART_OAUTH2_CLIENT_SECRET/g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s/SMART_OAUTH2_CLIENT_SECRET/$SMART_OAUTH2_CLIENT_SECRET/g" /usr/share/nginx/html/cat/main.js
 
-sed -i "s~FHIR_SERVER_URL~$FHIR_SERVER_URL~g" /usr/share/nginx/html/cat/main*bundle.js
+sed -i "s~FHIR_SERVER_URL~$FHIR_SERVER_URL~g" /usr/share/nginx/html/cat/main.js
 
 
 exec "$@"
