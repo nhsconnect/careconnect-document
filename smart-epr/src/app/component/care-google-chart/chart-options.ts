394ae50bb5553b2373c6721c24847e22b822f348
KevinMayfield <mayfield.g.kev@gmail.com>
2018-07-07 10:33:43 +0100

    Smart EHR App - Tidy

--- smart-epr/src/app/component/care-google-chart/chart-options.ts
+++
@@ -1,9 +0,0 @@
-import {ChartOptionsAxis} from './chart-options-axis';
-
-export class ChartOptions {
-  title :string;
-  width : number;
-  height : number;
-  xAxis : ChartOptionsAxis;
-  vAxis : ChartOptionsAxis;
-}
