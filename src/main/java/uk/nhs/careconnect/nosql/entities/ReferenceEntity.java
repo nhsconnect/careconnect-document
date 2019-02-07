8aafbc97bb82161576593a6b39adeba7d2a59834
David Knowles <david_robert_knowles@ymail.com>
2019-02-07 14:00:26 +0000

    ALP4-812 Refactored, renamed and added unit tests for some entity classes

--- src/main/java/uk/nhs/careconnect/nosql/entities/ReferenceEntity.java
+++ src/main/java/uk/nhs/careconnect/nosql/entities/ReferenceEntity.java
@@ -1,35 +1,33 @@
 package uk.nhs.careconnect.nosql.entities;
 
-public class Reference {
-    String reference;
-    String display;
-    IdentifierEntity identifier;
+import org.hl7.fhir.dstu3.model.Reference;
 
-    public Reference(org.hl7.fhir.dstu3.model.Reference subject) {
+public class ReferenceEntity {
+
+    private String reference;
+    private String display;
+    private IdentifierEntity identifier;
+
+    public ReferenceEntity() {
+
     }
 
+    public ReferenceEntity(Reference reference) {
+        this.reference = reference.getReference();
+        this.display = reference.getDisplay();
+        this.identifier = new IdentifierEntity(reference.getIdentifier());
+    }
 
     public String getReference() {
         return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
     }
 
     public String getDisplay() {
         return display;
     }
 
-    public void setDisplay(String display) {
-        this.display = display;
-    }
-
     public IdentifierEntity getIdentifier() {
         return identifier;
     }
 
-    public void setIdentifier(IdentifierEntity identifier) {
-        this.identifier = identifier;
-    }
 }
