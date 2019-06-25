46b27dbc70a11dd0c9deb79cd295bf35d44597c3
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-06-25 11:26:54 +0100

    Removed swagger ui component

--- src/main/java/uk/nhs/careconnect/nosql/DocumentRepository.java
+++ src/main/java/uk/nhs/careconnect/nosql/DocumentRepository.java
@@ -13,13 +13,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.nhs.careconnect.nosql.support.CustomCorsFilter;
 
 import java.time.Clock;
 
 @SpringBootApplication
-@EnableSwagger2
 public class DocumentRepository {
 
     @Autowired
